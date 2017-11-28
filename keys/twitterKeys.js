6f83d6230de702cd08409fe5de2057670628516a
sudame <su.glasses.923@gmail.com>
2017-11-28 21:51:29 +0900

    コールバックURLを変更

--- keys/twitterKeys.js
+++ keys/twitterKeys.js
@@ -1,5 +1,5 @@
 module.exports = {
     consumerKey: "gyObfNcOk1b5beZcrdDJV9Qit",
     consumerSecret: "hyBEMXEhDBc1AvyJEagQuvLSqFYxnYm0xLh1uoo8yHggZ02J26",
-    callbackURL: "http://localhost:3000/token"
+    callbackURL: "https://sarahah-generator.herokuapp.com/token"
 }
