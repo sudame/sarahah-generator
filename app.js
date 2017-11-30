b7468d5622719bf491d832648f3312accbcf3bed
sudame <su.glasses.923@gmail.com>
2017-11-30 11:29:32 +0900

    desirializeを修正

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ passport.serializeUser(function (user, done) {
 
 passport.deserializeUser(function (id, done) {
   const user = users[id];
-  done(null, user);
+  if(!null) done(null, user);
 });
 
 
