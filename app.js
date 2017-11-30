8993c0d069caac738f4f70d8201aae54aec151c0
sudame <su.glasses.923@gmail.com>
2017-11-30 13:12:25 +0900

    limitを変更

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser({limit: '5mb'}));
+app.use(bodyParser({ limit: '5mb' }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
