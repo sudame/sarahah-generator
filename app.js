b3c2ad574e412d3f53a34fdd63e655031ef07cd5
sudame <su.glasses.923@gmail.com>
2017-11-30 11:52:32 +0900

    誤記修正

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
+  users.findById(id, function (err, user) {
     done(err, user);
   });
 });
