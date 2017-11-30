e8baa8ceae8902b5d21a9b783ef7dc80d2133100
sudame <su.glasses.923@gmail.com>
2017-11-30 12:53:07 +0900

    ログイン判定基準を変更

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res, next) {
   req.session.text = req.query.text;
   console.log('INDEX.JS REQ.USER = ' + req.user);
   if(req.user) console.log(Object.keys(req.user));
-  if (req.user && req.user.profile && req.user.profile.id) {
+  if (req.user.id) {
     res.render('index', {login: true, title: req.user.profile.id });
   } else {
     res.render('index', {login: false, title: 'Express' });
